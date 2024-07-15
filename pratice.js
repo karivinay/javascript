b65e178b27f5ced83a4ddf4764f5cf96e802a5dc
karivinay <karivinay99@gmail.com>
2024-07-15 13:38:49 +0530

    new file added

---
+++ pratice.js
@@ -0,0 +1,18 @@
+let marks = prompt("enter marks");
+if(marks>0 & marks <= 49){
+    console .log("F grade")
+    }
+else if(marks> 49 & marks <= 59){
+    console .log("D grade")
+    }
+else if(marks> 59 & marks <= 69){
+        console .log("C grade")
+    }
+else if(marks> 69 & marks <= 89){
+        console .log("B grade")
+    }
+else if(marks> 89 & marks <= 100){
+        console .log("A grade")
+    }
+
+        
