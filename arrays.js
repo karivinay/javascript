f71b3ea90ca647df5c203c3897371f95a3d8e048
karivinay <karivinay99@gmail.com>
2024-07-17 13:14:44 +0530

    array and function file added

---
+++ arrays.js
@@ -0,0 +1,21 @@
+//  one dimensional Array
+ 
+//  let marks =[27,53,78,36,97];
+// console.log("one dimentional array ",marks);
+// console.log(marks.length)
+
+// let heroes=["iron man","hulk","spider man","vision","captain america","doctor strange"];
+// for(i=0;i<=heroes.length;i++){
+//     console.log(heroes[i]);
+// }
+// for(let hero of heroes){
+//     console.log(hero);
+// }
+// for(let hero in heroes){
+//     console.log(hero);
+// }
+
+
+// heroes.splice(3,1,'superman');
+
+
