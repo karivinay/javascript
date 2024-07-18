da594b21743462bb55fd1e37c900eb91e139cb68
karivinay <karivinay99@gmail.com>
2024-07-18 13:33:32 +0530

    function file added

---
+++ functions.js
@@ -0,0 +1,53 @@
+// function myfunction(){
+//     console.log("my name is vinay");
+//     console.log("workig at cinereous");
+
+// }
+
+// myfunction();
+
+
+// function cinereous(msg){
+//     console.log(msg);
+// }
+
+// cinereous("web development");
+
+// sum of two number
+// function sum(x,y){
+//     console.log(x +y);
+// }
+// sum(4,5)
+
+
+// arrow
+// const sum=(a,b)=>{
+//     console.log(a+b);
+// }
+
+
+// vowels count
+
+// function vowels(str){
+//     let count = 0;
+//     for(const char of str){
+//         if( 
+//             char === "a"||
+//             char === "e"||
+//             char === "i"||
+//             char === "o"||
+//             char === "u"
+//         ){count++
+
+//         }
+            
+//     }
+//     return count;
+// }
+
+
+// Array.for each
+let nums = [1,2,3,4,5,6];
+nums.forEach ((num) => {
+    console.log(num*num);
+});
