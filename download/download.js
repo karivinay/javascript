7fac8296615cd625de01fa679966b461140166b9
karivinay <karivinay99@gmail.com>
2024-07-27 15:57:14 +0530

    new file added

--- download/download.js
+++ download/download.js
@@ -1,28 +1,34 @@
-const downloadBtn = document.querySelector(".download-btn")
+// const downloadBtn = document.querySelector(".download-btn")
 
-const fileLink = "https://th.bing.com/th/id/OIP.VB_hRo4sruATXbq45qibhgHaEo?rs=1&pid=ImgDetMain";
+// const fileLink = "https://drive.google.com/file/d/1ttutxyl6ddn_UnzryZhej0FrAx9NDW_d/view?usp=sharing";
 
-const initTimer = () => {
-    if(downloadBtn.classList.contains("disable-timer")) {
-        return location.href = fileLink;
-    }
-    let timer = downloadBtn.dataset.timer;
-    downloadBtn.classList.add("timer");
-    downloadBtn.innerHTML = `Your download will begin in <b>${timer}</b> seconds`;
-    const initCounter = setInterval(() => {
-        if(timer > 0) {
-            timer--;
-            return downloadBtn.innerHTML = `Your download will begin in <b>${timer}</b> seconds`;
-        }
-        clearInterval(initCounter);
-        location.href = fileLink;
-        downloadBtn.innerText = "Your file is downloading...";
-        setTimeout(() => {
-            downloadBtn.classList.replace("timer", "disable-timer");
-            downloadBtn.innerHTML = `<i class="fa-solid fa-download" ></i>
-                                        <span class="text">Download Files</span>`;
-        }, 3000);
-    }, 1000);
-}
 
-downloadBtn.addEventListener("click", initTimer);
+
+// const initTimer = () => {
+//     if(downloadBtn.classList.contains("disable-timer")) {
+//         return location.href = fileLink;
+//     }
+//     let timer = downloadBtn.dataset.timer;
+//     downloadBtn.classList.add("timer");
+//     downloadBtn.innerHTML = `Your download will begin in <b>${timer}</b> seconds`;
+//     const initCounter = setInterval(() => {
+//         if(timer > 0) {
+//             timer--;
+//             return downloadBtn.innerHTML = `Your download will begin in <b>${timer}</b> seconds`;
+//         }
+//         clearInterval(initCounter);
+//         location.href = fileLink;
+//         downloadBtn.innerText = "Your file is downloading...";
+//         setTimeout(() => {
+//             downloadBtn.classList.replace("timer", "disable-timer");
+//             downloadBtn.innerHTML = `<i class="fa-solid fa-download" ></i>
+//                                         <span class="text">Download Files</span>`;
+//         }, 3000);
+//     }, 1000);
+// }
+
+// downloadBtn.addEventListener("click", initTimer);
+
+
+
+
